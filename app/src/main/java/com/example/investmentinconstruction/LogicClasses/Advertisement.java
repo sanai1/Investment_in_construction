a4c36a9019961b7d13b11384e9aa4d224b8bdcc4
aleksandr <sanek.v.07@gmail.com>
2024-10-09 03:29:30 +0300

    add prevHouse in Advertisement and update fragment_other_players.xml (layout_width)

--- app/src/main/java/com/example/investmentinconstruction/LogicClasses/Advertisement.java
+++ app/src/main/java/com/example/investmentinconstruction/LogicClasses/Advertisement.java
@@ -6,15 +6,18 @@ public class Advertisement implements Serializable {
 
     private Integer house;
     private Integer shop;
+    private Integer prevHouse;
 
     public Advertisement() {
         this.house = 0;
         this.shop = 0;
+        this.prevHouse = 0;
     }
 
-    public Advertisement(Integer house, Integer shop) {
+    public Advertisement(Integer house, Integer shop, Integer prevHouse) {
         this.house = house;
         this.shop = shop;
+        this.prevHouse = prevHouse;
     }
 
     public Integer getHouse() {
@@ -31,5 +34,13 @@ public class Advertisement implements Serializable {
 
     public void setShop(Integer shop) {
         this.shop = shop;
+    }
+
+    public Integer getPrevHouse() {
+        return prevHouse;
+    }
+
+    public void setPrevHouse(Integer prevHouse) {
+        this.prevHouse = prevHouse;
     }
 }
